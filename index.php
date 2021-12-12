233b242b835a7277c8620d921a1a23afe67fef2f
root <root@DESKTOP-N2014V3.localdomain>
2021-12-12 15:21:00 +0800

    First commint

---
+++ index.php
@@ -0,0 +1,5 @@
+<?PHP
+	echo "WELCOME";
+	echo " When using the command prompt to install PHP, make sure you run it as Administrator. Simply run the command install php7 or any other 	    available php version on the machine. Simply enter the command php version to see what version of PHP is installed on your computer. Finally	, following the installation, you may begin coding.";
+
+?>
